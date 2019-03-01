1d0b84634435afaed94d554e2846b7990f70c4bb
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2019-03-01 15:08:45 -0800

    add colorpicker example

---
+++ inst/examples/colorpicker.R
@@ -0,0 +1,50 @@
+library(vuer)
+
+html_dependencies_vue_color <- function(){
+  htmltools::htmlDependency(
+    name = 'vue-color',
+    version = '2.7.0',
+    src = c(href = 'https://unpkg.com/vue-color/dist'),
+    script = 'vue-color.min.js'
+  )
+}
+
+vueColorPickerInput <- function(type = types, ...){
+  types <- c('compact', 'chrome', 'swatches', 'sketch')
+  types %>%
+    purrr::map(~ {
+      .x %>%
+        stringi::stri_trans_totitle() %>%
+        paste0('VueColor.', .)
+    }) %>%
+    rlang::set_names(paste0(types, '-picker')) %>%
+    vueComponents$register(.list = .)
+  type <- match.arg(type)
+  htmltools::tag(
+    sprintf("%s-picker", type), list("v-model" = "colors", ...)
+  ) %>%
+    appendDependencies(list(html_dependencies_vue_color()))
+}
+
+
+
+ui <- div(
+  vueColorPickerInput(type = "sketch"),
+  plotOutput('plot')
+) %>%
+  Vue(
+    data = list(colors_ = '#fff')
+  )
+
+server <- function(input, output, session){
+   output$plot <- renderPlot({
+     mycolor <- if (is.null(input$colors)) 'black' else input$colors$hex[1]
+     ggplot(mtcars, (aes(x = wt, y = mpg))) +
+       geom_point(color = mycolor)
+   })
+   output$colors <- renderText({
+     input$colors$hex[1]
+   })
+}
+
+shinyApp(ui = ui, server = server)
